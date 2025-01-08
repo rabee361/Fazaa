601df76b7894d12f67adce3b45be923aeb7d1241
rabee361 <rha60540@gmail.com>
2025-01-08 19:43:12 +0300

    validator chack and limit on number of images and reels

--- Fazaa/settings.py
+++ Fazaa/settings.py
@@ -127,10 +127,10 @@ AUTH_USER_MODEL = 'users.CustomUser'
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'fazaa_db',
+        'NAME': 'postgres',
         'USER': 'postgres', 
-        'PASSWORD': 'rabee123',
-        'HOST': 'localhost',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
