d65e042bb25bd5510340fb4d0069920cfce02a51
rabee361 <rha60540@gmail.com>
2025-03-10 21:53:00 +0300

    fixes

--- utils/notifications.py
+++ utils/notifications.py
@@ -1,9 +1,8 @@
-from users.models import User , Client , Shareek
+from users.models import User
 from django.db.models import Q
 from users.models import UserType
 from fcm_django.models import FCMDevice
-from firebase_admin.messaging import Message, Notification
-from firebase_admin.messaging import UnregisteredError , ErrorInfo
+from firebase_admin.messaging import Message, Notification , UnregisteredError
 from firebase_admin.exceptions import InvalidArgumentError
 
 def send_users_notification(title,body,recipient_type):
@@ -13,17 +12,17 @@ def send_users_notification(title,body,recipient_type):
         users = User.objects.filter(Q(is_active=True) & Q(user_type=UserType.CLIENT))
     elif recipient_type == 'shareeks':
         users = User.objects.filter(Q(is_active=True) & Q(user_type=UserType.SHAREEK))
-    for user in users:
-        devices = FCMDevice.objects.filter(user=user)
-        for device in devices:
-            try:
-                device.send_message(Message(
-                    notification=Notification(
-                        title=title,
-                        body=body
-                    )
-                ))
-            except UnregisteredError as e:
-                pass
-            except InvalidArgumentError as e:
-                pass
+
+    devices = FCMDevice.objects.filter(user__in=users)
+    for device in devices:
+        try:
+            device.send_message(Message(
+                notification=Notification(
+                    title=title,
+                    body=body
+                )
+            ))
+        except UnregisteredError as e:
+            pass
+        except InvalidArgumentError as e:
+            pass
