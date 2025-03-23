9702b4b7073e224216d7ec67193378ce4c0aeffb
rabee361 <rha60540@gmail.com>
2025-03-23 16:17:19 +0300

    fix

--- Fazaa/asgi.py
+++ Fazaa/asgi.py
@@ -8,14 +8,15 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Fazaa.settings')
+
 from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from users.middleware import JWTAuthMiddlewareStack , JWTAuthMiddleware
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Fazaa.settings')
-django_asgi_app = get_asgi_application()
+from users.middleware import JWTAuthMiddlewareStack, JWTAuthMiddleware
 import users.routing
 
 application = ProtocolTypeRouter({
