71e693ea0d66846535048e5006c704cc60eff092
rabee361 <rha60540@gmail.com>
2025-08-15 11:17:15 +0300

    f

--- Fazaa/settings.py
+++ Fazaa/settings.py
@@ -141,13 +141,24 @@ ASGI_APPLICATION = 'Fazaa.asgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'fazaa_db',
+        'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': 'db',
         'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.contrib.gis.db.backends.postgis',
+#         'NAME': 'fazaa_db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'rabee123',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 if ENVIRONMENT:
