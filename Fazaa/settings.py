147dd7bd08a7b3a2b1ec5385aa465900669fed2e
rabee361 <rha60540@gmail.com>
2025-08-15 11:36:45 +0300

    f

--- Fazaa/settings.py
+++ Fazaa/settings.py
@@ -31,7 +31,7 @@ environ.Env.read_env(os.path.join(BASE_DIR , 'Fazaa', '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG', False)
+DEBUG = env('DEBUG', default='False')
 
 ENVIRONMENT = env('ENVIRONMENT')
 
