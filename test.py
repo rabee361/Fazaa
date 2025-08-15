df977ad26875bff6ea6746d1aa8800f1656b8ae6
rabee361 <rha60540@gmail.com>
2025-08-15 18:54:50 +0300

    fix

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 # class Shape():
-#     def __init__(self):
-#         self.
+#     def __init__(self, x):
+#         self.x = x
 
 
 # class Circle(Shape):
 #     def calc_diameter(self):
-#         return self.
+#         return self.x
 
 # class Square(Shape):
 #     def calc(self):
-#         return self.
+#         return self.x
