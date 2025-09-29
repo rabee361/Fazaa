98103728622d73fa248e631c0562c024bf323b0e
rabee361 <rha60540@gmail.com>
2025-09-29 23:59:27 +0300

    fixes

--- Fazaa/settings.py
+++ Fazaa/settings.py
@@ -257,8 +257,8 @@ if os.name == 'nt':  # for Windows
 
 # jwt settings
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=4),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=3),
+    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=5),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
